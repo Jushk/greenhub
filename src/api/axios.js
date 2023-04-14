826ef848b67489ee2669df74ed6f39f47c3909fa
Justinn <justinepicaze@gmail.com>
2023-04-14 16:06:44 +0800

    Ready to deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3500/api/v1";
+const BASE_URL = "https://greenhub-api.onrender.com/api/v1";
 
 const authInstance = axios.create({
   baseURL: `${BASE_URL}/auth`,
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if(process.env.NODE_ENV == 'production') disableReactDevTools()
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
